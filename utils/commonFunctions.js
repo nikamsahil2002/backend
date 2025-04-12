b9454a9cfabe21224e51c6c39d2b47c772806939
SahilNikam <sahilnikam@nimapinfotech.com>
2025-04-12 16:33:13 +0530

    added mail functionality for overdue tasks

--- utils/commonFunctions.js
+++ utils/commonFunctions.js
@@ -34,37 +34,42 @@ const checkIfRecordExist = async(collection, _id) => {
   return result ? true : false;
 }
 
-const taskNotification = async(taskId) => {
-  // get task recurrence from taskId
-  const task = await db.task_recurrence
-              .find({ taskId })
-              .populate("assignedTo")
+const taskNotification = async(taskId, isOverDue) => {
+  try{
+      // get task recurrence from taskId
+    const task = await db.task_recurrence
+                .find({ taskId })
+                .populate("assignedTo")
 
-  const title = task[0].title;
-  const description = task[0].description;
-  const media = task[0].media;  // will change the path in future.
-  const startDate = task[0].startDate;
-  const dueDate = task[0].dueDate;
-  
-  const notificationMailTemplate = await db.email_template.find({name: "task-notiification" });
+    const title = task[0].title;
+    const description = task[0].description;
+    const media = task[0].media;  // will change the path in future.
+    const startDate = task[0].startDate;
+    const dueDate = task[0].dueDate;
+    const mailType = isOverDue ? 'task-over-due' : "task-notiification";
+    
+    const notificationMailTemplate = await db.email_template.find({name: mailType });
 
-  let mailBody = notificationMailTemplate[0].body;
+    let mailBody = notificationMailTemplate[0].body;
 
-  mailBody = mailBody.replace("{{title}}", title);
-  mailBody = mailBody.replace("{{description}}", description);
-  mailBody = mailBody.replace("{{media}}", media);
-  mailBody = mailBody.replace("{{startDate}}", startDate);
-  mailBody = mailBody.replace("{{dueDate}}", dueDate);
+    mailBody = mailBody.replace("{{title}}", title);
+    mailBody = mailBody.replace("{{description}}", description);
+    mailBody = mailBody.replace("{{media}}", media);
+    mailBody = mailBody.replace("{{startDate}}", startDate);
+    mailBody = mailBody.replace("{{dueDate}}", dueDate);
 
-  // send notification to all assigned users
-  for(let i=0;i<task[0].assignedTo.length;i++){
-    sendEmail(
-      task[0].assignedTo[i].email,
-      notificationMailTemplate[0].subject,
-      mailBody
-    );
+    // send notification to all assigned users
+    for(let i=0;i<task[0].assignedTo.length;i++){
+      sendEmail(
+        task[0].assignedTo[i].email,
+        notificationMailTemplate[0].subject,
+        mailBody
+      );
+    }
   }
-
+  catch(err){
+    console.log(err)
+  }
 }
 
 module.exports = {
--- utils/cron/cronDaily.js
+++ utils/cron/cronDaily.js
@@ -3,61 +3,100 @@ const moment = require("moment");
 const commonFunction = require('../commonFunctions');
 
 exports.cronDaily = async () => {
-    const tasks = await db.task.find().handleWeeklyTasklean();
+    const tasks = await db.task.find({
+        recurrence: { $ne: "once" }
+    }).lean();
+
     const dailyTasks = tasks.filter((t) => t.recurrence == "daily");
     const weeklyTasks = tasks.filter((t) => t.recurrence == "weekly");
     const monthlyTasks = tasks.filter((t) => t.recurrence == "montly");
 
-
     handleDailyTask(dailyTasks);
     handleWeeklyTask(weeklyTasks);
     handleMonthlyTask(monthlyTasks);
+
+    const overDueTasks = await db.task_recurrence.find({
+        dueDate: { $lte : moment().format("YYYY-MM-DD HH:mm:ss") }
+    });
+    console.log(overDueTasks)
+    handleOverDueTask(overDueTasks);
 }
 
 function handleDailyTask(tasks) {
-    const bodyArray = tasks.map((t) => {
-        const { createdAt, updatedAt, recurrence, ...rest } = t;
-        rest.taskId = t._id;
-        return rest;
-    });
-
-    db.task_recurrence.create(bodyArray);
-    for(let i=0;i<tasks.length;i++){
-        commonFunction.taskNotification(tasks[i]._id);
+    try{
+        const bodyArray = tasks.map((t) => {
+            const { _id, createdAt, updatedAt, recurrence, startDate, ...rest } = t;
+            return {
+                ...rest,
+                taskId: t._id,
+                dueDate: moment().add(moment.duration(t.estimatedTime || 0, 'hours')).toISOString(),
+                startDate: moment().toISOString()
+            }
+        });
+        db.task_recurrence.create(bodyArray);
+        for(let i=0;i<tasks.length;i++){
+            commonFunction.taskNotification(tasks[i]._id);
+        }
+    }
+    catch(err){
+        console.log(err)
     }
 }
 
 function handleWeeklyTask(tasks) {
-    const oneWeekAgo = moment().subtract(7, 'days');
-
-    const filteredTasks = tasks.filter(t => moment(t.createdAt).isBefore(oneWeekAgo));
-
-    const bodyArray = filteredTasks.map(t => {
-        const { createdAt, updatedAt, recurrence, ...rest} = t;
-        rest.taskId = t._id;
-        return rest;
-    })
-
-    db.task_recurrence.create(bodyArray);
-    for(let i=0;i<tasks.length;i++){
-        commonFunction.taskNotification(tasks[i]._id);
+    try{
+        const oneWeekAgo = moment().subtract(7, 'days');
+        const filteredTasks = tasks.filter(t => moment(t.createdAt).isBefore(oneWeekAgo));
+        const bodyArray = tasks.map((t) => {
+            const { _id, createdAt, updatedAt, recurrence, startDate, ...rest } = t;
+            return {
+                ...rest,
+                taskId: t._id,
+                dueDate: moment().add(moment.duration(t.estimatedTime || 0, 'hours')).toISOString(),
+                startDate: moment().toISOString()
+            }
+        });
+        db.task_recurrence.create(bodyArray);
+        for(let i=0;i<tasks.length;i++){
+            commonFunction.taskNotification(tasks[i]._id);
+        }
+    }
+    catch(err){
+        console.log(err)
     }
 }
 
 function handleMonthlyTask(tasks) {
-    const oneMonthAgo = moment().subtract(1, 'months');
+    try{
+        const oneMonthAgo = moment().subtract(1, 'months');
+        const filteredTasks = tasks.filter(t => moment(t.createdAt).isBefore(oneMonthAgo));
+        const bodyArray = tasks.map((t) => {
+            const { _id, createdAt, updatedAt, recurrence, startDate, ...rest } = t;
+            return {
+                ...rest,
+                taskId: t._id,
+                dueDate: moment().add(moment.duration(t.estimatedTime || 0, 'hours')).toISOString(),
+                startDate: moment().toISOString()
+            }
+        });
+        db.task_recurrence.create(bodyArray);
+        for(let i=0;i<tasks.length;i++){
+            commonFunction.taskNotification(tasks[i]._id);
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
 
-    const filteredTasks = tasks.filter(t => moment(t.createdAt).isBefore(oneMonthAgo));
-
-    const bodyArray = filteredTasks.map((t) => {
-        const { createdAt, updatedAt, recurrence, ...rest } = t;
-        rest.taskId = t._id;
-        return rest;
-    });
-
-    db.task_recurrence.create(bodyArray);
-    for(let i=0;i<tasks.length;i++){
-        commonFunction.taskNotification(tasks[i]._id);
+function handleOverDueTask(overDueTasks){
+    try{
+        for(let i=0;i<overDueTasks.length;i++){
+            commonFunction.taskNotification(overDueTasks[i].taskId, true);
+        }
+    }
+    catch(err){
+        console.log(err)
     }
 }
 
